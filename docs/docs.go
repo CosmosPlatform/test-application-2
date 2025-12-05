16bb4255d74e61cbc0d7b4d066ff1c4cecc440d7
RafaB15 <rberenguel@fi.uba.ar>
2025-12-05 19:43:55 -0300

    Update swagger

--- docs/docs.go
+++ docs/docs.go
@@ -215,6 +215,10 @@ const docTemplate = `{
         "api.Order": {
             "type": "object",
             "properties": {
+                "additional_info": {
+                    "type": "string",
+                    "example": "Gift wrap requested"
+                },
                 "created_at": {
                     "type": "string",
                     "example": "2023-01-01T00:00:00Z"
@@ -265,10 +269,6 @@ const docTemplate = `{
         "api.OrderUpdateRequest": {
             "type": "object",
             "properties": {
-                "demo_attribute": {
-                    "type": "string",
-                    "example": "demo-value"
-                },
                 "metadata": {
                     "type": "object",
                     "additionalProperties": {
