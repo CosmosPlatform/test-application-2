e10f8699e6b57a49fa9b21c1650414ef6c38cb85
RafaB15 <rberenguel@fi.uba.ar>
2025-12-05 08:41:34 -0300

    Update Order dto

--- docs/docs.go
+++ docs/docs.go
@@ -227,6 +227,10 @@ const docTemplate = `{
                     "type": "string",
                     "example": "customer-456"
                 },
+                "demo_attribute": {
+                    "type": "string",
+                    "example": "demo-value"
+                },
                 "id": {
                     "type": "string",
                     "example": "order-123"
